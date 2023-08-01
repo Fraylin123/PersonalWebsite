58968fd664cb5b58615a98efd6ad3d6e963eac93
Fraylin Ayala <fraylinayala321@gmail.com>
2023-07-31 20:51:09 -0400

    Added files

---
+++ main.js
@@ -0,0 +1,15 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+var scrolled = (winScroll / height) * 99.9;
+ document.getElementById("myBar").style.width = scrolled + "%";
+
+}
+
+
+
+
+        
+        
