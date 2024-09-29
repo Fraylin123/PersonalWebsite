e40ba00463ef5ea23d1ce5910c8f29dcefa4902d
Fraylin <fraylinayala321@gmail.com>
2024-09-29 14:24:16 -0400

    updates

--- main.js
+++ main.js
@@ -18,9 +18,21 @@ function myFunction() {
     if (currentProjectNum < 5) {
       currentProjectNum++;
     }
-
-
+    
     if (currentProjectNum == 2) {
+      
+      imageLink.href = "https://fraylin123.github.io/pocketGuard/";
+      imageLink.target = "_blank";
+
+      document.getElementById("projectText").innerHTML = " Pocketguard is a website that helps the user manage their debts and save money on their online service subscriptions. Created during the summer of my junior year as the culminating project of the All Star Code summer program.";
+      document.getElementById("projectImage").src = "imgs/logo.png";
+      document.getElementById("projectImage").style.width = "330px";
+
+
+    }
+
+
+    if (currentProjectNum == 3) {
       imageLink.href = "https://github.com/Fraylin123/Stocks-Web-Parser";
       imageLink.target = "_blank";
 
@@ -36,7 +48,17 @@ function myFunction() {
       currentProjectNum--;
     }
 
-    if (currentProjectNum == 1) {
+
+    if (currentProjectNum == 1){
+      document.getElementById("projectText").innerHTML = "I made this calculator app during an online advanced FDU Python Course using the Tkinter GUI Kit";
+      document.getElementById("projectImage").src = "imgs/calculator.png"; 
+      imageLink.href = "https://replit.com/@FraylinA/Calculator-App";
+      imageLink.target = "_blank";
+      document.getElementById("projectImage").style.width = "220px";
+     
+    }
+    if (currentProjectNum == 2) {
+      
       imageLink.href = "https://fraylin123.github.io/pocketGuard/";
       imageLink.target = "_blank";
 
